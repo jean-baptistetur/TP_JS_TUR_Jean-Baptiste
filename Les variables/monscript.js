40fff93999dada0658aba3b77bd7b8dddacef404
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-04 11:24:07 +0100

    TP6 : LES OPÉRATEURS ARITHMÉTIQUES

--- Les variables/monscript.js
+++ Les variables/monscript.js
@@ -1,57 +1,16 @@
+let nbr1 = 3;
+let nbr2 = 7;
+let nbr3 = 8;
+let nbr4 = 21;
 
-let nomJour = "Mercredi";
-let numJour = 4;
-let nomMois = "Février";
-let isHiver = true;
+console.log(nbr1 + nbr2);
+console.log(nbr4 - nbr3);
 
-console.log(nomJour);
-console.log(numJour);
-console.log(nomMois);
-console.log(isHiver);
+nbr2 = nbr1 * nbr2;
+console.log(nbr2);
 
+nbr2++;
+console.log(nbr2);
 
-let tabJours = [
-  "Lundi",
-  "Mardi",
-  "Mercredi",
-  "Jeudi",
-  "Vendredi",
-  "Samedi",
-  "Dimanche"
-];
-
-
-let tabMois = [
-  "Janvier",
-  "Février",
-  "Mars",
-  "Avril",
-  "Mai",
-  "Juin",
-  "Juillet",
-  "Août",
-  "Septembre",
-  "Octobre",
-  "Novembre",
-  "Décembre"
-];
-
-
-console.log(tabJours[2]);
-
-
-console.log(tabMois[1]);
-
-
-let maDateV1 = {
-  jour: nomJour,
-  mois: nomMois
-};
-
-
-let maDateV2 = new Object();
-maDateV2.jour = tabJours[2];
-maDateV2.mois = tabMois[1];
-
-console.log(maDateV1);
-console.log(maDateV2);
+console.log(nbr4 / nbr1);
+console.log(nbr4 % nbr1);
