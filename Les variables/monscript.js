883b5d4f7b032c4cf8a831a054e5c59ced887e4c
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-04 11:48:36 +0100

    fin du code sur les Variables

--- Les variables/monscript.js
+++ Les variables/monscript.js
@@ -1,3 +1,91 @@
+//TP 4 Type de données basiques : 
+// let nomMois = "Février";
+// let isHiver = true;
+
+
+// console.log(nomJour);
+// console.log(numJour);
+// console.log(nomMois);
+// console.log(isHiver);
+
+
+// TP 5 : Type de données avancées
+
+// let nomJour = "Mercredi";
+// let numJour = 4;
+// let nomMois = "Février";
+// let isHiver = true;
+
+// console.log(nomJour);
+// console.log(numJour);
+// console.log(nomMois);
+// console.log(isHiver);
+
+// let tabJours = [
+//   "Lundi",
+//   "Mardi",
+//   "Mercredi",
+//   "Jeudi",
+//   "Vendredi",
+//   "Samedi",
+//   "Dimanche"
+// ];
+
+
+// let tabMois = [
+//   "Janvier",
+//   "Février",
+//   "Mars",
+//   "Avril",
+//   "Mai",
+//   "Juin",
+//   "Juillet",
+//   "Août",
+//   "Septembre",
+//   "Octobre",
+//   "Novembre",
+//   "Décembre"
+// ];
+
+
+// console.log(tabJours[2]);
+
+
+// console.log(tabMois[1]);
+
+
+// let maDateV1 = {
+//   jour: nomJour,
+//   mois: nomMois
+// };
+
+
+// let maDateV2 = new Object();
+// maDateV2.jour = tabJours[2];
+// maDateV2.mois = tabMois[1];
+
+// console.log(maDateV1);
+// console.log(maDateV2);
+
+
+//TP 6 : LES OPÉRATEURS ARITHMÉTIQUES
+// let nbr1 = 3;
+// let nbr2 = 7;
+// let nbr3 = 8;
+// let nbr4 = 21;
+
+// console.log(nbr1 + nbr2);
+// console.log(nbr4 - nbr3);
+// nbr2 = nbr1 * nbr2;
+// console.log(nbr2);
+
+// nbr2++;
+// console.log(nbr2);
+
+// console.log(nbr4 / nbr1);
+// console.log(nbr4 % nbr1);
+
+// TP 7 :  La concatenation
 let nomSerie;
 let nomPerso;
 
