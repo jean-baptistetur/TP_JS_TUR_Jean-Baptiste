b2de3bc194990eb8c22277678661981f5b165182
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-04 11:28:37 +0100

    TP7 : La concatenation

--- Les variables/monscript.js
+++ Les variables/monscript.js
@@ -1,16 +1,7 @@
-let nbr1 = 3;
-let nbr2 = 7;
-let nbr3 = 8;
-let nbr4 = 21;
+let nomSerie;
+let nomPerso;
 
-console.log(nbr1 + nbr2);
-console.log(nbr4 - nbr3);
+nomSerie = prompt("Quelle est ta série préférée ?");
+nomPerso = prompt("Quelle est ton personnage favori ?");
 
-nbr2 = nbr1 * nbr2;
-console.log(nbr2);
-
-nbr2++;
-console.log(nbr2);
-
-console.log(nbr4 / nbr1);
-console.log(nbr4 % nbr1);
+alert(`Ma série préférée est ${nomSerie} et mon personnage préféré est ${nomPerso} !!`);
