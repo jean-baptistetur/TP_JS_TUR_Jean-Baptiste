13f8a716799f7adede470a809805676e1efcb278
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-04 11:14:18 +0100

    TP 4 des variables

---
+++ Les variables/monscript.js
@@ -0,0 +1,11 @@
+
+let nomJour = "Mercredi";
+let numJour = 4;
+let nomMois = "Février";
+let isHiver = true;
+
+
+console.log(nomJour);
+console.log(numJour);
+console.log(nomMois);
+console.log(isHiver);
