22ff25c87ae27a81b65a508b0024231573e44dbc
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-04 12:09:34 +0100

    TP10 : ELSE IF ET SWITCH

--- Les conditions/monscript.js
+++ Les conditions/monscript.js
@@ -17,12 +17,61 @@
 
 
 //TP9 : LES OPÉRATEURS LOGIQUES
-let age = Number(prompt("Renseignez votre âge : "));
+// let age = Number(prompt("Renseignez votre âge : "));
 
-if (age < 18) {
-    alert(`L’utilisateur a ${age} an(s), il est mineur !`);
-} else if (age >= 62) {
-    alert(`L’utilisateur a ${age} ans, il est majeur mais aussi retraité !!`);
-} else {
-    alert(`L’utilisateur a ${age} ans, il est majeur !`);
+// if (age < 18) {
+//     alert(`L’utilisateur a ${age} an(s), il est mineur !`);
+// } else if (age >= 62) {
+//     alert(`L’utilisateur a ${age} ans, il est majeur mais aussi retraité !!`);
+// } else {
+//     alert(`L’utilisateur a ${age} ans, il est majeur !`);
+// }
+
+
+//TP 10 : ELSE IF ET SWITCH
+
+// avec IF et ELSE
+// let age = Number(prompt("Renseignez votre âge :"));
+
+// if (age < 18) {
+//     alert(`L’utilisateur a ${age} ans, il est mineur !`);
+// } else if (age === 18) {
+//     alert("Il vient d’être majeur");
+// } else if (age === 25) {
+//     alert("Il a un quart de siècle");
+// } else if (age === 50) {
+//     alert("Il a un demi siècle");
+// } else if (age === 62) {
+//     alert("Il vient d’être à la retraite");
+// } else if (age === 100) {
+//     alert("Il vient d’être centenaire");
+// } else {
+//     alert(`L’utilisateur a ${age} ans, il est majeur !`);
+// }
+
+// Avec switch 
+let age = Number(prompt("Renseignez votre âge :"));
+
+switch (age) {
+    case 18:
+        alert("Il vient d’être majeur");
+        break;
+    case 25:
+        alert("Il a un quart de siècle");
+        break;
+    case 50:
+        alert("Il a un demi siècle");
+        break;
+    case 62:
+        alert("Il vient d’être à la retraite");
+        break;
+    case 100:
+        alert("Il vient d’être centenaire");
+        break;
+    default:
+        if (age < 18) {
+            alert(`L’utilisateur a ${age} ans, il est mineur !`);
+        } else {
+            alert(`L’utilisateur a ${age} ans, il est majeur !`);
+        }
 }
