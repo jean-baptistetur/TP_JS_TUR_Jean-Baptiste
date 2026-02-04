d2e486cd0796cb6ee6b261c7afb1eb747517bf3f
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-04 11:58:56 +0100

    TP8 : LES OPÉRATEURS DE COMPARAISON

---
+++ Les conditions/monscript.js
@@ -0,0 +1,16 @@
+//TP 8 :
+let number = 5;
+let text = '5';
+let isRainingToday = true;
+
+if(number == text){
+    console.log("Vrai pour les valeurs");
+}else{
+    console.log("Faux pour les valeurs");
+}
+
+if(number === text){
+    console.log("Vrai pour les valeurs et le type");
+}else{
+    console.log("Faux pour les valeurs ou le type");
+}
