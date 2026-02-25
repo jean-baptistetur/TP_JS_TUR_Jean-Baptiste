df58fb877b9805bec54e621596e283643dc2cb6a
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-25 11:26:44 +0100

    TP14 : Liste des courses - Classes CSS

--- Les manipulations du DOM/script.js
+++ Les manipulations du DOM/script.js
@@ -3,9 +3,8 @@ let li = document.querySelector("#listeCourses li");
 
 /* Tous les évènements */
 li.addEventListener('click', function() {
-  // Changement de la couleur de fond et texte barré
-  this.style.backgroundColor = "lightblue";
-  this.style.textDecoration = "line-through";
+  // On bascule la classe CSS (TP14)
+  this.classList.toggle("itemCheck");
 });
 
 /* Les fonctions */
