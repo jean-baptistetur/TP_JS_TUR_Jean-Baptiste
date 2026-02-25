3ddd167e14fa5badb1524f857094bb13f7f32879
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-02-25 11:32:27 +0100

    TP16 : Liste des courses - Ajouter éléments HTML

--- Les manipulations du DOM/script.js
+++ Les manipulations du DOM/script.js
@@ -1,18 +1,41 @@
-
-let li = document.querySelector("#listeCourses li");
-
-
 let dateJour = new Date(Date.now());
 let dateJourFr = dateJour.toLocaleDateString('fr-FR');
-
-
 let titre = document.querySelector("h2");
-
-
 titre.textContent = titre.textContent + " : " + dateJourFr;
 
+let myInput = document.querySelector("#myInput");
+let btn = document.querySelector(".addBtn");
+let liste = document.querySelector("#listeCourses");
+let premierLi = document.querySelector("#listeCourses li");
 
+premierLi.addEventListener('click', function() {
+    this.classList.toggle("itemCheck");
+});
 
-li.addEventListener('click', function() {
-  this.classList.toggle("itemCheck");
+function addProduct() {
+    let valeur = myInput.value;
+
+    if (valeur === "") {
+        alert("Erreur de saisie");
+    } else {
+        alert("Produit ajouté");
+        
+        let nouveauLi = document.createElement("li");
+        nouveauLi.textContent = valeur;
+        
+        nouveauLi.addEventListener('click', function() {
+            this.classList.toggle("itemCheck");
+        });
+        
+        liste.appendChild(nouveauLi);
+        myInput.value = "";
+    }
+}
+
+btn.addEventListener('click', addProduct);
+
+myInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addProduct();
+    }
 });
